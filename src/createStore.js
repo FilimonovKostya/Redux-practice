99f725254ea5657bf1a08f491b059aae42086a95
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-10-23 00:39:57 +0300

    initial commit

---
+++ src/createStore.js
@@ -0,0 +1,17 @@
+export function createstore(rootReducer) {
+    let state = {}
+    const subscribers = []
+
+    return {
+        dispatch(action) {
+            state = rootReducer(state, action)
+            subscribers.forEach(sub=> sub())
+        },
+        subscribe(callback) {
+            subscribers.push(callback)
+        },
+        getState() {
+            return state
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './styles.css'
+import {createstore} from "./createStore";
+
+const counter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub')
+const asyncBtn = document.getElementById('async')
+const themeBtn = document.getElementById('theme')
+
+const store = createstore()
+
+addBtn.addEventListener('click', () => {
+
+})
+
+subBtn.addEventListener('click', () => {
+
+})
+
+asyncBtn.addEventListener('click', () => {
+
+})
+
+themeBtn.addEventListener('click', () => {
+    // document.body.classList.toggle('dark')
+})
